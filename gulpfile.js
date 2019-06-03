61f168a8382f1d593cf2738aa16301d5e38722cb
吉川　宗志 <soyoshik@yahoo-corp.jp>
2019-06-04 00:02:45 +0900

    ejs構築

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,14 @@ var $             = require('gulp-load-plugins')();
 var browserSync   = require('browser-sync');
 const babel       = require('gulp-babel');
 
-var paths = {
+var path = {
   "htmlSrc" : "./*.html",
+  "ejsSrc"  : "./src/ejs/",
   "scssSrc" : "./src/scss/**/*.scss",
   "jsSrc"   : "./src/js/**/*.js",
   "es6Src"  : "./src/es6/**/*.es6",
   "imgSrc"  : "./src/images/**",
-  "dest"    : "./dest/",
+  "dest"    : "./dest/"
 }
 
 
@@ -27,21 +28,28 @@ gulp.task('bs-reload', function() {
   browserSync.reload();
 });
 
+gulp.task("ejs", function() {
+    gulp.src([path.ejsSrc + "*.ejs", path.ejsSrc + "**/*.ejs", "!" + path.ejsSrc + "**/_*.ejs"])
+    .pipe($.plumber())
+    .pipe($.ejs('',{"ext": ".html"}))
+    .pipe(gulp.dest(path.dest + 'html'));
+});
+
 gulp.task('scss', function() {
-  return gulp.src(paths.scssSrc)
+  return gulp.src(path.scssSrc)
   .pipe($.sourcemaps.init())
   .pipe($.sass()).on('error', $.sass.logError)
   .pipe($.autoprefixer({
     browsers: ['last 2 versions']
   }))
   .pipe($.sourcemaps.write())
-  .pipe(gulp.dest(paths.dest + 'css'))
+  .pipe(gulp.dest(path.dest + 'css'))
   .pipe($.autoprefixer())
   .pipe($.rename({
     suffix: '.min'
   }))
   .pipe($.csso())
-  .pipe(gulp.dest(paths.dest + 'css'))
+  .pipe(gulp.dest(path.dest + 'css'))
   .pipe(browserSync.reload({
     stream: true,
     once  : true
@@ -50,12 +58,13 @@ gulp.task('scss', function() {
 
 
 gulp.task('image', function() {
-  return gulp.src(paths.imgSrc)
-  .pipe($.changed(paths.dest + 'images'))
+  return gulp.src(path.imgSrc)
+  .pipe($.plumber())
+  .pipe($.changed(path.dest + 'images'))
   .pipe($.imagemin({
     optimizationLevel: 3
   }))
-  .pipe(gulp.dest(paths.dest + 'images'))
+  .pipe(gulp.dest(path.dest + 'images'))
   .pipe(browserSync.reload({
     stream: true,
     once  : true
@@ -63,11 +72,11 @@ gulp.task('image', function() {
 });
 
 gulp.task('js', function() {
-  return gulp.src([paths.jsSrc])
+  return gulp.src([path.jsSrc])
   .pipe($.plumber())
   .pipe($.uglify({preserveComments: 'license'}))
   .pipe($.concat('mainNormal.min.js', {newLine: '\n'}))
-  .pipe(gulp.dest(paths.dest + 'js'))
+  .pipe(gulp.dest(path.dest + 'js'))
   .pipe(browserSync.reload({
     stream: true,
     once  : true
@@ -75,34 +84,38 @@ gulp.task('js', function() {
 });
 
 gulp.task('babel', function() {
-  return gulp.src([paths.es6Src])
+  return gulp.src([path.es6Src])
   .pipe($.plumber())
   .pipe(babel({
       presets: ['@babel/env']
   }))
   .pipe($.uglify({preserveComments: 'license'}))
   .pipe($.concat('mainEs6.min.js', {newLine: '\n'}))
-  .pipe(gulp.dest(paths.dest + 'js'))
+  .pipe(gulp.dest(path.dest + 'js'))
   .pipe(browserSync.reload({
     stream: true,
     once  : true
   }));
 });
 
-gulp.task('default', ['image', 'js', 'bs', 'scss', 'bs-reload','babel'], function() {
-  $.watch([paths.htmlSrc],function(e) {
+gulp.task('default', ['image', 'ejs', 'js', 'bs', 'scss', 'bs-reload','babel'], function() {
+  $.watch([path.htmlSrc],function(e) {
     gulp.start("bs-reload")
   });
-  $.watch([paths.scssSrc],function(e) {
+  $.watch([path.ejsSrc+"*.ejs", path.ejsSrc+"**/*.ejs"],function(e) {
+    gulp.start("ejs");
+    gulp.start("bs-reload");
+  });
+  $.watch([path.scssSrc],function(e) {
     gulp.start("scss")
   });
-  $.watch([paths.imgSrc],function(e) {
+  $.watch([path.imgSrc],function(e) {
     gulp.start("image")
   });
-  $.watch([paths.jsSrc],function(e) {
+  $.watch([path.jsSrc],function(e) {
     gulp.start("js")
   });
-  $.watch([paths.es6Src],function(e) {
+  $.watch([path.es6Src],function(e) {
     gulp.start("babel")
   });
 });
